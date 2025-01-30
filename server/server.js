03d00b1946225588c95dd14419525aa7b60ca5ff
cattalurdai <cattalurdai@gmail.com>
2025-01-30 18:10:49 -0300

    feat(server): create dummy request routes

--- server/server.js
+++ server/server.js
@@ -1,2 +1,35 @@
+const express = require("express");
+const server = express();
+const port = 3000
 
-console.log("hello world")
+// Get tasks
+server.get("/tasks", (req,res) => {
+    res.send("Get tasks");
+})
+
+// Create task
+server.post("/tasks", (req,res) => {
+    res.send("Create tasks");
+})
+
+// Create task
+server.delete("/tasks/:id", (req,res) => {
+    
+    const taskId= req.params.id;
+
+    res.send("Deleted task with id: " + taskId);
+})
+
+
+// Create task
+server.put("/tasks/:id", (req,res) => {
+    
+    const taskId= req.params.id;
+
+    res.send("Updated task with id: " + taskId);
+})
+
+// START SERVER
+server.listen(port, ()=>{
+    console.log("Server started on port:",port);
+})
